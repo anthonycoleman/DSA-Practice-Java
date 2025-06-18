4330238a03d2c1b01dd15fedb8736a18ae85bd1d
Anthony Coleman <anthonycoleman@MacBookAir.lan>
2025-06-17 22:21:35 -0400

    feat: Implement searching algorithms solutions

--- src/main/java/com/dsa/algorithms/searching/Searching.java
+++ src/main/java/com/dsa/algorithms/searching/Searching.java
@@ -11,8 +11,15 @@ public class Searching {
      * @return The index of the target if found, otherwise -1.
      */
     public int linearSearch(int[] arr, int target) {
-        // TODO: Implement Linear Search
-        return -1;
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Target found
+            }
+        }
+        return -1; // Target not found
     }
 
     /**
@@ -24,7 +31,29 @@ public class Searching {
      * @return The index of the target if found, otherwise -1.
      */
     public int binarySearch(int[] arr, int target) {
-        // TODO: Implement Binary Search
-        return -1;
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if target is present at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // If target greater, ignore left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Target not present in array
     }
 }
